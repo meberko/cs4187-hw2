6c1fe80cdae954508cdcf89cac7fae3128544646
meberko <m.e.berkowitz@gmail.com>
2016-11-14 17:57:20 -0500

    derp, forgot a semicolon

--- main.c
+++ main.c
@@ -157,4 +157,5 @@ int main(int argc, char* argv[]){
             openAndExecuteFile(fname);
         }
     }
+    return 0;
 }
--- password.c
+++ password.c
@@ -3,5 +3,5 @@
 int main(int argc, char* argv[]){
     printf("Running password\n");
     //for(int i=0; i<argc; i++) printf("%s\n", argv[i]);
-    return 0
+    return 0;
 }
